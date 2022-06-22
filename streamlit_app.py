a91c6f510f8bdd54004ea52a7d9c3c582831c970
Abhishek-Baid <33655961+Abhishek-Baid@users.noreply.github.com>
2022-06-22 18:48:02 +0530

    Update streamlit_app.py
    Creating dataframe for the api call and showing it

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,12 @@ streamlit.dataframe(fruit_to_show)
 
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#streamlit.text(fruityvice_response.json())
+
+#Using pandas to convert json to a table like format (dataframe)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+#Output dataframe to screen
+streamlit.dataframe(fruityvice_normalized)
+
