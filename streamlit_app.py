b1d7ceda0fb38bcdf4f0b5891317b48ab68f2ff2
Abhishek-Baid <33655961+Abhishek-Baid@users.noreply.github.com>
2022-06-22 22:32:32 +0530

    Update streamlit_app.py
    Adding insert into fruit_load_list command

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,3 +51,7 @@ streamlit.dataframe(my_data_rows)
 #Taking fruit choice from user
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+
+# Highlighting control flow issue
+my_cur.execute("Insert into fruit_load_list values ('from_streamlit')")
